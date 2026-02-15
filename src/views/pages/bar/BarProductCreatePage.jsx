1a3ae54eebde998791c0658a3cad67c4eb4ae192
toure <kesseguillaume@gmail.com>
2026-02-15 09:08:20 -0500

    Route bar vers version produitBar + restauration écrans gérant

---
+++ src/views/pages/bar/BarProductCreatePage.jsx
@@ -0,0 +1,14 @@
+import ProductCreatePage from "../produitBar/ProductCreatePage";
+
+export default function BarProductCreatePage() {
+  return (
+    <ProductCreatePage
+      title="Catalogue Bar"
+      breadcrumbs={[
+        { title: "Bar", path: "/accueil/bar/ventes" },
+        { title: "Catalogue" },
+      ]}
+      defaultTab={0} // Ajout classique par défaut (formulaire complet)
+    />
+  );
+}
